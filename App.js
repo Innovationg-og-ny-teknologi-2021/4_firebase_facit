e622af49d1ecafe4318c9d169953ff3fab2bea33
pelolsen <pelolsen@hotmail.com>
2023-09-10 20:37:10 +0200

    Facit

--- App.js
+++ App.js
@@ -1,13 +1,96 @@
+import React, {useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Importere Firebase Services
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { Card } from 'react-native-paper';
+
+//Importere vores componenter fra components mappe
+import ProfileScreen from './components/ProfileScreen';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCno9TvjNJSfFpSLphe0IEEv8R8ykXor1s",
+  authDomain: "oevelse4-c59b0.firebaseapp.com",
+  projectId: "oevelse4-c59b0",
+  storageBucket: "oevelse4-c59b0.appspot.com",
+  messagingSenderId: "515052009733",
+  appId: "1:515052009733:web:eb3ad4a3fbf146432adf6c",
+  measurementId: "G-SBV2N6EH87"
+};
+
+// Initialize Firebase
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [user, setUser] = useState({ loggedIn: false });
+
+  if (getApps().length < 1) {
+    initializeApp(firebaseConfig);
+    console.log("Firebase On!");
+    // Initialize other firebase products here
+  } else {
+    console.log("Firebase not on!");
+  }
+ 
+  const auth = getAuth();
+
+  function onAuthStateChange(callback) {
+    return onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        callback({loggedIn: true, user: user});
+        console.log("You are logged in!");
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        callback({loggedIn: false});
+      }
+    });
+  }
+
+ //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //Her oprettes gæstekomponentsindhold, der udgøres af sign-up og login siderne
+  const GuestPage = () => {
+    return(
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>
+            Opret eller Login med din firebase Email
+          </Text>
+          
+          <Card style={{padding:20, margin: 20}}>
+            <SignUpForm />
+          </Card>
+          
+          <Card style={{padding:20, margin: 20}}>
+            <LoginForm />
+          </Card>
+
+        </View>
+    )
+  }
+
+
+
+
+  return user.loggedIn ? <ProfileScreen /> : <GuestPage/> ;
+
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +99,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
